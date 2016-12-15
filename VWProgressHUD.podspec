ebc9406000ca5b1ea8868d2337506fcd55189bd4
Vaith <Vaith@WitchNaNadeMac-mini.local>
2016-12-15 11:20:44 +0800

    fix bug

--- VWProgressHUD.podspec
+++ VWProgressHUD.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'VWProgressHUD'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A easy hud to use, just one code'
 
 # This description is used to generate tags and improve search results.
--- VWProgressHUD/Classes/VWConfig.h
+++ VWProgressHUD/Classes/VWConfig.h
@@ -4,6 +4,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #define VWHEXCOLOR(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
